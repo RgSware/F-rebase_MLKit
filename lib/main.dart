1332bdfadc8262cff100a15119ad14359b016538
RgSware <rahimgokceli78@gmail.com>
2020-10-04 19:49:20 +0300

    Flutter App

---
+++ lib/main.dart
@@ -0,0 +1,194 @@
+import 'package:mm/ml_detail.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+const String TEXT_SCANNER = 'TEXT_SCANNER';
+const String BARCODE_SCANNER = 'BARCODE_SCANNER';
+const String LABEL_SCANNER = 'LABEL_SCANNER';
+const String FACE_SCANNER = 'FACE_SCANNER';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MLHome(),
+  ));
+}
+
+class MLHome extends StatefulWidget {
+  MLHome({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _MLHomeState();
+}
+
+class _MLHomeState extends State<MLHome> {
+  static const String CAMERA_SOURCE = 'CAMERA_SOURCE';
+  static const String GALLERY_SOURCE = 'GALLERY_SOURCE';
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  File _file;
+  String _selectedScanner = TEXT_SCANNER;
+
+  @override
+  Widget build(BuildContext context) {
+    final columns = List<Widget>();
+    columns.add(buildRowTitle(context, 'Recognition Type'));
+    columns.add(buildSelectScannerRowWidget(context));
+    columns.add(buildtext(context));
+    columns.add(buildSelectImageRowWidget(context));
+
+    return Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.cyan,
+          title: Text('MlHub'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: columns,
+          ),
+        ));
+  }
+
+  Widget buildtext(BuildContext context) {
+    final rows = List<Widget>();
+    rows.add(buildRowTitle(context, 'Take a Picture'));
+    rows.add(buildRowTitle(context, ' '));
+    rows.add(buildRowTitle(context, 'Pick Image'));
+    return Row(
+      children: rows,
+    );
+  }
+
+  Widget buildRowTitle(BuildContext context, String title) {
+    return Center(
+        child: Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline,
+      ),
+    ));
+  }
+
+  Widget buildSelectImageRowWidget(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+            child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          child: RaisedButton(
+              color: Colors.cyan,
+              textColor: Colors.white,
+              splashColor: Colors.blueGrey,
+              onPressed: () {
+                onPickImageSelected(CAMERA_SOURCE);
+              },
+              child: const Icon(Icons.camera)),
+        )),
+        Expanded(
+            child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          child: RaisedButton(
+              color: Colors.cyan,
+              textColor: Colors.white,
+              splashColor: Colors.blueGrey,
+              onPressed: () {
+                onPickImageSelected(GALLERY_SOURCE);
+              },
+              child: const Icon(Icons.image)),
+        ))
+      ],
+    );
+  }
+
+  Widget buildSelectScannerRowWidget(BuildContext context) {
+    return Wrap(
+      children: <Widget>[
+        RadioListTile<String>(
+          title: Text('Text Recognition'),
+          groupValue: _selectedScanner,
+          value: TEXT_SCANNER,
+          onChanged: onScannerSelected,
+        ),
+        RadioListTile<String>(
+          title: Text('Barcode Recognition'),
+          groupValue: _selectedScanner,
+          value: BARCODE_SCANNER,
+          onChanged: onScannerSelected,
+        ),
+        RadioListTile<String>(
+          title: Text('Face Recognition'),
+          groupValue: _selectedScanner,
+          value: FACE_SCANNER,
+          onChanged: onScannerSelected,
+        )
+      ],
+    );
+  }
+
+  Widget buildImageRow(BuildContext context, File file) {
+    return SizedBox(
+        height: 500.0,
+        child: Image.file(
+          file,
+          fit: BoxFit.fitWidth,
+        ));
+  }
+
+  Widget buildDeleteRow(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+        child: RaisedButton(
+            color: Colors.red,
+            textColor: Colors.white,
+            splashColor: Colors.blueGrey,
+            onPressed: () {
+              setState(() {
+                _file = null;
+              });
+              ;
+            },
+            child: const Text('Delete Image')),
+      ),
+    );
+  }
+
+  void onScannerSelected(String scanner) {
+    setState(() {
+      _selectedScanner = scanner;
+    });
+  }
+
+  void onPickImageSelected(String source) async {
+    var imageSource;
+    if (source == CAMERA_SOURCE) {
+      imageSource = ImageSource.camera;
+    } else {
+      imageSource = ImageSource.gallery;
+    }
+
+    final scaffold = _scaffoldKey.currentState;
+
+    try {
+      final file = await ImagePicker.pickImage(source: imageSource);
+      if (file == null) {
+        throw Exception('File is not available');
+      }
+
+      Navigator.push(
+        context,
+        new MaterialPageRoute(
+            builder: (context) => MLDetail(file, _selectedScanner)),
+      );
+    } catch (e) {
+      scaffold.showSnackBar(SnackBar(
+        content: Text(e.toString()),
+      ));
+    }
+  }
+}
